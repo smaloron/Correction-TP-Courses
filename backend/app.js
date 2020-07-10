e7059cd027bea29f87d35fabd7b44a56623267f5
Sebastien Maloron <contact@smaloron.eu>
2020-07-10 10:03:55 +0200

    Mise en place du serveur Express

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+// Création de l'application
+const app = require('express')();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Référencement des middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Lancement du serveur
+app.listen(3002, () => console.log('server started'));
