fbbab870d91d289aed454a02456f8a9230a8ea30
Sebastien Maloron <contact@smaloron.eu>
2020-07-10 14:06:42 +0200

    Affichage des catégories

--- backend/controllers.js
+++ backend/controllers.js
@@ -1,8 +1,19 @@
 const articleDAO = require('./models/article-model');
-const listeDAO = require('./models/list-model');
+const listDAO = require('./models/list-model');
 const categoryDAO = require('./models/category-model');
 
 exports.getAllArticles = async (req, res) => {
   const result = await articleDAO.findAll();
+  await res.status(200).json(result);
+};
+
+exports.getAllArticlesByList = async (req, res) => {
+  const listId = req.params.listId;
+  const result = await articleDAO.findAllByListId(listId);
+  await res.status(200).json(result);
+};
+
+exports.getAllCategories = async (req, res) => {
+  const result = await categoryDAO.findAll();
   await res.status(200).json(result);
 };
--- backend/routes.js
+++ backend/routes.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const controller = require('./controllers');
 
 router.get('/article', controller.getAllArticles);
+router.get('/article-list/:listId([0-9]+)', controller.getAllArticlesByList);
+router.get('/category', controller.getAllCategories);
 
 module.exports = router;
